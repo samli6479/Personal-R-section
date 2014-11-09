f0fd88e2c755cb2d58bce187a5d01d08a415464c
samli6479 <samli6479@gmail.com>
2014-11-08 23:33:43 -0500

    R code for learning purpose

---
+++ Edward.R
@@ -0,0 +1,54 @@
+library(MASS)
+library(ISLR)
+names(Boston)
+?Boston
+plot(medv~lstat,Boston)
+fit1=lm(medv~lstat,data=Boston)
+fit1
+summary(fit1)
+abline(fit1,col="red")
+names(fit1)
+confint(fit1)
+predict(fit1,data.frame(lstat=c(5,10,15)),interval="confidence")
+fit2=lm(medv~lstat+age,data=Boston)
+fit2
+summary(fit2)
+fit3=lm(medv~.,Boston)
+fit3
+summary(fit3)
+par(mfrow=c(2,2))
+plot(fit3)
+fit4=update(fit3,~.-age-indus)
+fit4
+summary(fit4)
+fit5=lm(medv~lstat*age,Boston)
+fit5
+summary(fit5)
+fit6=lm(medv~lstat+I(lstat^2),Boston);fit6;summary(fit6)
+attach(Boston)
+par(mfrow=c(1,1))
+plot(medv~lstat)
+points(lstat,fitted(fit6),col="red",pch=20)
+fit7=lm(medv~poly(lstat,4))
+points(lstat,fitted(fit7),col="blue",pch=20)
+plot(1:20,1:20,pch=1:20,cex=2)
+fix(Carseats)
+names(Carseats)
+summary(Carseats)
+Fit1=lm(Sales~.+Income:Advertising+Age:Price,Carseats)
+summary(Fit1)
+contrasts(Carseats$ShelveLoc)##Show dummay variable
+####  R function
+regplot=function(x,y){
+  fit=lm(y~x)
+  plot(x,y)
+  abline(fit,col="red")
+}
+attach(Carseats)
+regplot(Price,Sales)
+regplot=function(x,y,...){
+  fit=lm(y~x)
+  plot(x,y,...)
+  abline(fit,col="red")
+}
+regplot(Price,Sales,xlab="Price",ylab="Sales",col="blue",pch=20)
